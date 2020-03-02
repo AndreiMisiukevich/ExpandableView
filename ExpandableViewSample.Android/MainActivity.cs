68c01f785adda0cd72e84c1c28f31dd49772307f
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-03-02 17:07:22 +0300

    removed useless packages from sample

--- ExpandableViewSample.Android/MainActivity.cs
+++ ExpandableViewSample.Android/MainActivity.cs
@@ -6,7 +6,6 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using TouchEffect.Droid;
 
 namespace ExpandableViewSample.Droid
 {
--- ExpandableViewSample.iOS/AppDelegate.cs
+++ ExpandableViewSample.iOS/AppDelegate.cs
@@ -4,7 +4,6 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
-using TouchEffect.iOS;
 
 namespace ExpandableViewSample.iOS
 {
